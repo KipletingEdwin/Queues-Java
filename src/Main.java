a74f748e13f7b7db6f8a752fa60fa092818e59b5
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-01-20 09:44:59 +0000

    Add a Queue of String datatype

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         // FIFO    =     First In First Out
+
+        Queue<String> queue = new LinkedList<String>();
 
     }
 }
